553250f600b5571f408127d1a85d7d5fb9dcdc86
Joseph Bello <josephodion1@gmail.com>
2018-10-26 12:18:26 -0600

    Added some code to class ListElement

--- src/ListElement.java
+++ src/ListElement.java
@@ -1,5 +1,27 @@
 
 public class ListElement
-{
+{	
+		public ListElement next;
+		//private ListEl
+		private int data;
+		
+		
+		public ListElement()
+		{
+			this.next = null;
+			this.data = 0;
+			
+		}
+		
+		
+		public void setData(int data)
+		{
+			this.data = data;
+		}
 
+		
+		public int getData()
+		{
+			return this.data;
+		}
 }
