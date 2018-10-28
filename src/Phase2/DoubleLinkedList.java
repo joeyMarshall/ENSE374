1e60a6407581f502e96437557bcd3f8d9e894066
Joseph Bello <josephodion1@gmail.com>
2018-10-27 21:08:44 -0600

    completed phase 1 and phase 2 of the assignment

--- src/Phase2/DoubleLinkedList.java
+++ src/Phase2/DoubleLinkedList.java
@@ -1,7 +1,43 @@
+package Phase2;
 import assignment2.*;
 
-
 public class DoubleLinkedList {
+		
+		
+		public static void main(String[] args)
+		{
+			// TODO Auto-generated method stub
+	ListElement le = new ListElement();
+	LinkedList list = new LinkedList();
+	System.out.format("List added\n");
 
-}
+
+	//access the members of the class using the object le
+	le.setData(0);
+	list.addElement(le);
+	le.setData(1);
+	list.addElement(le);
+	le.setData(2);
+	list.addElement(le);
+	le.setData(3);
+	list.addElement(le);
+	le.setData(4);
+	list.addElement(le);
+
+	ListElement value = new ListElement();
+	value=list.getElement(3);
+	System.out.format("The value is: " + value.getData() + "\n");
+
+	list.printLinkedListHead();
+
+	list.deleteElement(3);
+	list.printLinkedListHead();
+
+	System.out.format("The value is set to 5\n");
+	System.out.println(le.getData());
+		}
+	}
+
+
+
 
--- src/assignment2/LinkedList.java
+++ src/assignment2/LinkedList.java
@@ -1,42 +1,99 @@
 package assignment2;
 
-public class LinkedList
+public class LinkedList {
+	   
+    private ListElement head;
+    private ListElement tail;
+    private int count=0;
+    private int data;
+
+
+public void addElement(ListElement le)
 {
-	private ListElement head;
-	private ListElement tail;
-	private int count = 0;
-	private int data;
-	
-	public static void main(String[] args)
-	{
-		// TODO Auto-generated method stub
-ListElement le = new ListElement();
-LinkedList list = new LinkedList();
-System.out.format("The value is:, args");
+        ListElement tempPtr;
+            tempPtr = new ListElement(null, le.getData());
+     
+        if (head == null)
+        {
+            head = tempPtr;
+            tail = head;
+        }
+        else
+        {
+            tail.setNext(tempPtr);
+            tail = tempPtr;
+        }
+        incrementCounter();
+    }
 
+public  void incrementCounter()
+{
+    count++;
+}   
 
-//access the members of the class using the object le
-le.setData(0);
-mylist.addElement(le);
-le.setData(1);
-mylist.addElement(le);
-le.setData(2);
-mylist.addElement(le);
-le.setData(3);
-mylist.addElement(le);
-le.setData(4);
-mylist.addElement(le);
-
-ListElement value = new ListElement();
-value=mylist.getElement(3);
-System.out.format("The value is: " + value.getData() + "\n");
-
-mylist.printLinkedListHead();
-
-mylist.deleteElement(3);
-mylist.printLinkedListHead();
-
-System.out.format("The value is set to 5\n");
-System.out.println(le.getData());
-	}
+private void decrementCounter()
+{
+    count--;
 }
+
+@SuppressWarnings("null")
+public ListElement getElement(int index)
+// returns the element at the specified position in this list.
+{
+   
+    ListElement CurrPtr = new ListElement();
+    if (head != null)
+    {
+        CurrPtr = head;
+        for (int i = 0; i < index; i++)
+        {
+            if (CurrPtr.getNext() == null)
+                {return null;}
+            CurrPtr = CurrPtr.getNext();
+        }
+        return CurrPtr;
+    }
+    return null;
+}
+    // removes the element at the specified position in this list.
+    public ListElement deleteElement(int index)
+    {
+        ListElement NumPtr = head;
+        ListElement TemPtr = null;
+
+        if (!(head == null && tail == null))
+        {// Delete first  node
+            if (index == 0)
+            {
+                NumPtr = NumPtr.getNext();
+                NumPtr.setNext(NumPtr.getNext());
+                head = NumPtr;
+            }
+            else
+            {
+            
+                for (int i = 1; i < index; i++)
+                    NumPtr = NumPtr.getNext();
+              
+                // Delete node at location->next
+                TemPtr = NumPtr.getNext();
+                NumPtr.setNext(NumPtr.getNext().getNext());
+            }
+        }  
+        return null;
+    }  
+   
+public void printLinkedListHead()
+{
+    ListElement currentPtr = new ListElement();
+    //ListElement newElement = new ListElement();
+    currentPtr=head;
+    while(currentPtr != null)
+    {
+        System.out.println("List Element from head are: " + currentPtr.getData() + "\n");
+        currentPtr = currentPtr.getNext();
+    }
+}
+
+}
+
